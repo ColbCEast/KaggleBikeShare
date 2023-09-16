1528505df8a534cd939551d7fdf2cf8759436935
Colby Eastmond <colby.eastmond@gmail.com>
2023-09-15 20:53:49 -0600

    Linear Regression Modification

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -4,22 +4,45 @@ library(tidymodels)
 library(dplyr)
 
 bike_data <- vroom("train.csv")
-view(bike_data)
 
 # dyplyr Cleaning Step
 bike_data_clean <- bike_data %>%
   mutate(weather = ifelse(weather == 4, 3, weather))
+bike_data <- bike_data %>%
+  select(-casual, -registered)
+new_bike_data <- vroom("test.csv")
 
 # Feature Engineering Step
-my_bike_data <- recipe(count~., data = bike_data_clean) %>%
+my_bike_recipe <- recipe(count~., data = bike_data) %>%
   step_num2factor(weather, levels = c("Clear", "Cloudy/Misty", "Light Snow/Rain")) %>%
   step_date(datetime, features="dow") %>%
-  step_rm(casual) %>%
-  step_rm(registered) %>%
   step_num2factor(season, levels = c("Spring", "Summer", "Fall", "Winter")) %>%
+  step_mutate(weather, levels = ifelse(weather == 4, 3, weather)) %>%
   step_mutate(holiday = ifelse(holiday == 0, "No", "Yes")) %>%
-  step_mutate(workingday = ifelse(workingday == 0, "No", "Yes"))
+  step_mutate(workingday = ifelse(workingday == 0, "No", "Yes")) %>%
+  step_time(datetime, features = "hour")
   
-prepped_data <- prep(my_bike_data)
-bike_data_wrangle <- bake(prepped_data, new_data = bike_data)
-view(bike_data_wrangle)
+prepped_data <- prep(my_bike_recipe)
+bake(prepped_data, new_data = bike_data)
+bake(prepped_data, new_data = new_bike_data)
+
+
+# Linear Regression Section
+my_mod <- linear_reg() %>%
+  set_engine("lm")
+
+bike_workflow <- workflow() %>%
+  add_recipe(my_bike_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data = bike_data)
+
+bike_predictions <- predict(bike_workflow,
+                            new_data = new_bike_data) %>%
+  bind_cols(., new_bike_data) %>%
+  select(datetime, .pred) %>%
+  rename(count = .pred) %>%
+  mutate(count = pmax(0, count)) %>%
+  mutate(datetime = as.character(format(datetime)))
+vroom_write(x = bike_predictions, file = "TestPreds.csv", delim = ",")
+
+
